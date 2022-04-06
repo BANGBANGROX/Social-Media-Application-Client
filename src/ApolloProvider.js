d1ad4b03826fbad596449cf9e22c58667845450f
BANGBANGROX <lakshya0809bang@gmail.com>
2022-04-06 15:19:59 +0530

    Edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://fast-lowlands-66265.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
